1e635f8cae04cebdf22f8065d829ca99b7d6f95e
片岡亮平 <kataokaryohei@kataokaryouheinoMacBook-Air.local>
2026-01-06 17:08:16 +0900

    update top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ padding: 24, fontFamily: "sans-serif" }}>
       <h1>kaitori-matome</h1>
-      <p>編集できました！</p>
+      <p>起動テストOK！</p>
     </main>
   );
 }
